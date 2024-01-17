394ec43dd60b215ab847a99470baffa2c8e436fe
Wycliffe <odundoklife1@gmail.com>
2024-01-17 23:10:23 +0300

    added sending a signal email on registration

--- meetups/signals.py
+++ meetups/signals.py
@@ -1,8 +1,17 @@
-from os import name
+import email
+from email import message
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User, Group
 from .models import Paticipants
+
+# email
+import smtplib
+subject = 'success'
+message = "You have successfully signed up to Let's meet."
+
+text = f'subject:{subject}\n\n{message}'
+
 
 
 @receiver(post_save, sender=User)
@@ -13,7 +22,18 @@ def create_paticipant(sender, instance, created, **kwargs):
 
         Paticipants.objects.create(user=instance, email=instance.email)
 
+        # email
+        email = "odundoklife1@gmail.com"
+        receiver_email = 'pythonklife@gmail.com'
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email, 'cprp utbz lvbk xzxc')
+        server.sendmail(email, receiver_email, text)
+
         print('profile created')
+        # cprp utbz lvbk xzxc
+
 
 
 # post_save.connect(create_paticipant, sender=User)
